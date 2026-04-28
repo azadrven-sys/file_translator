f0188af71acf21925268898d7a8bff160c14b787
azadrven-sys <aza.d.r.ven@gmail.com>
2026-04-28 07:44:21 +0300

    Update and rename json_translator.bat to file_translator.bat

---
+++ file_translator.bat
@@ -0,0 +1,1 @@
+powershell -NoProfile -ExecutionPolicy Bypass -File file_translator.ps1 -help tr -InputDirectory .\* -OutputDirectory .\result -InputLanguage en -OutputLanguage tr -SkipOnError
--- json_translator.bat
+++
@@ -1,1 +0,0 @@
-powershell -NoProfile -ExecutionPolicy Bypass -File json_translator.ps1 -help tr -InputDirectory .\* -OutputDirectory .\result -InputLanguage en -OutputLanguage tr -SkipOnError
